f9545283deed1bb41b01524dcfc6b7c0710a8097
Sadi Burak Karakuş <karakus.sadiburak@gmail.com>
2025-08-02 19:32:04 +0300

    İlk taslak section 1

---
+++ src/News.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function News() {
+    return (
+        <div className="news-container">
+            <h2>Latest News</h2>
+            <ul>
+                <li>News item 1</li>
+                <li>News item 2</li>
+                <li>News item 3</li>
+            </ul>
+        </div>
+    );
+}
+export default News;
